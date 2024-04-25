6d39d10caa63a5418649f8c0009d8af786b3934e
Syeda Nabila <161962167+SyedaNabila559@users.noreply.github.com>
2024-04-25 09:08:29 -0800

    Add files via upload
    word count

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+// Import the 'inquirer' module, which is a command line interface for Node.js
+
+import inquirer from "inquirer";
+
+// Print a welcome message
+
+console.log ("Welcome tp code with Nabila word count")
+
+// Declare a constant 'answers' assign it in to the result of the inquirer.prompt function
+
+const answer: {
+     sentence: string
+} = await inquirer.prompt([
+     {
+       name: "sentence",
+       type: "input",
+       message: "Enter a sentence to count the words",
+           
+       },
+]);
+
+const words = answer.sentence.trim().split(" ")
+
+// Print the message of Array to the console
+
+console.log(words);
+
+// Print the word count of sentence of the console
+
+console.log(`Your sentence word count is ${words.length}`);
+
+
+
+
